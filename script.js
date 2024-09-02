ac93021c304481a394bf66463b74dfddf9446c05
jorge <jorgebandeo+Github@hotmail.com>
2024-09-02 19:59:40 -0300

    adição da modelagem inicial
    paginas base e estruturação dos dados, contatos funcionais

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleNavOverlay() {
+    var nav = document.getElementById("navMenu");
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "block";
+    }
+}
